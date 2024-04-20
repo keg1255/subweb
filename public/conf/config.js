790b30a61c55108b1b728a2f744ba411d1f11cd4
keg1255 <18782071219@163.com>
2024-04-21 00:26:06 +0800

    Update config.js

--- public/conf/config.js
+++ public/conf/config.js
@@ -2,7 +2,7 @@ window.config = {
   // 网站标题
   siteName: 'Subconverter Web',
   // API 地址
-  apiUrl: 'http://127.0.0.1:25500',
+  apiUrl: 'https://subweb.q2g.one',
   // 短域名服务地址
   shortUrl: 'https://s.ops.ci',
   // 默认远程配置
